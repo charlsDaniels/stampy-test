9d2f03b59a74f9410f620b615f6d70df6ce22f1b
greatCharles <menendez_carlos@yahoo.com.ar>
2021-04-16 09:43:35 -0300

    borrado de función para dd

--- index.php
+++ index.php
@@ -6,13 +6,6 @@ session_start();
 
 $action=isset($_GET['action']) ? $_GET['action'] : 'view_login';
 
-function dd($data){
-  $format = print_r('<pre>');
-  $format .= print_r($data);
-  $format .= print_r('</pre>');
-  echo $format;
-}
-
 try {
   Dispatcher::$action();
 } catch (\Throwable $t) {
